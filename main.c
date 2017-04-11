c7b1c1166d8f716b54391c63d7d220c57ec033ae
Naftaly Avadiaev <naftaly.avadiaev@gmail.com>
2017-04-11 23:56:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,12 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-void* map;
-int f;
-struct stat st;
+void* map_passwd;
+void* map_dirty_wb;
+int f1, f2;
+struct stat st1, st2;
 
 char etc_passwd[] = "/etc/passwd";
 
@@ -22,10 +24,11 @@ void* madviseThread(void *arg)
   char *str = (char*)arg;
   int i,c=0;
 
-  for(i=0;i<100000;i++)
-    c+=madvise(map,100,MADV_DONTNEED);
-
-  printf("madvise: Completed %d\n\n",c);
+  for(i=0;i<100000;i++) {
+    c+=madvise(map_passwd,100,MADV_DONTNEED);
+	c+=madvise(map_dirty_wb,100,MADV_DONTNEED);
+  }
+  //printf("madvise: Completed %d\n\n",c);
 }
  
 void* procselfmemThread(void *arg)
@@ -36,11 +39,13 @@ void* procselfmemThread(void *arg)
   int i,c=0;
 
   for(i=0;i<100000;i++) {
-    lseek(f,(uintptr_t) map,SEEK_SET);
+    lseek(f1,(uintptr_t) map_passwd,SEEK_SET);
     c+=write(f,str,strlen(str));
+    lseek(f2,(uintptr_t) map_dirty_wb,SEEK_SET);
+    c+=write(f,0,1);
   }
 
-  printf("/proc/self/mem: Completed %d\n\n", c);
+  //printf("/proc/self/mem: Completed %d\n\n", c);
 }
  
  
@@ -49,16 +54,22 @@ int main(int argc,char *argv[])
   pthread_t pth1,pth2;
 
 
-  f=open(etc_passwd,O_RDONLY);
-  fstat(f,&st);
+  f1=open(etc_passwd,O_RDONLY);
+  fstat(f1,&st1);
 
-  map=mmap(NULL,st.st_size,PROT_READ,MAP_PRIVATE,f,0);
-  printf("mmap %zx\n\n",(uintptr_t) map);
+  f2=open("/proc/sys/vm/dirty_writeback_centisecs",O_RDONLY);
+  fstat(f2,&st2);
+
+
+  map_passwd = mmap(NULL,st1.st_size,PROT_READ,MAP_PRIVATE,f1,0);
+  map_dirty_wb = mmap(NULL,st2.st_size,PROT_READ,MAP_PRIVATE,f2,0);
+
 
 
   pthread_create(&pth1,NULL,madviseThread, &etc_passwd);
   pthread_create(&pth2,NULL,procselfmemThread, &passwd);
   pthread_join(pth1,NULL);
   pthread_join(pth2,NULL);
+
   return 0;
 }
